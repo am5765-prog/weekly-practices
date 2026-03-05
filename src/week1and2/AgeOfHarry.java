f5878f613ba565d2d4fbf7bb9290e676cf6246ac
Anvi Mehta <am5765@srmist.edu.in>
2026-03-05 18:33:08 +0530

    weekly-practices

---
+++ src/week1and2/AgeOfHarry.java
@@ -0,0 +1,12 @@
+package week1and2;
+
+public class AgeOfHarry {
+    public static void main(String[] args) {
+        int birthYear = 2000;
+        int currentYear = 2026;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in 2026 is " + age);
+    }
+}
